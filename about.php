75134c0c8cba20baf23310ed1315a4cebe34d5a3
Nitesht17 <106956622+Nitesht17@users.noreply.github.com>
2024-05-10 14:42:49 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+
+    
+    <link rel="stylesheet" href="style.css">
+
+
+</head>  
+<body>
+
+<!-- header section start -->
+
+<section class="header">
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+
+<!-- header section end -->
+
+<div class="heading" style="background: url(images/background-1.jpg) no-repeat">
+<h1>about us</h1>
+</div>
+
+<!--- about section starts -->
+
+<section class="about">
+    <div class="image">
+        <img src="images/about-image.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptatibus quod placeat numquam laboriosam, amet inventore laudantium quam velit fugiat sed perspiciatis similique molestiae officia beatae blanditiis praesentium natus quasi.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Ex, incidunt. A facere exercitationem assumenda molestiae nobis ratione quod rem, 
+             architecto voluptate cumque laboriosam labore fugit corrupti sit impedit
+              autem est!</p>
+              <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>top destinations</span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>affordable price</span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24/7 guid service</span>
+                </div>
+              </div>
+    </div>
+</section>
+
+<!--- about section ends -->
+
+<!-- reviews section starts -->
+
+<section class="reviews">
+    <div class=" swiper reviews-slider">
+        
+    <div class="swiper-wrapper">
+
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+             veritatis quidem sapiente labore corporis ratione numquam laborum quasi, totam ducimus tempora
+             ex repellendus quisquam ullam, delectus magni nam perferendis est?</p>
+             <h3>john deo</h3>
+             <span>traveler</span>
+             <img src="images/pic-1.jpg" alt="">
+    </div>
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+             veritatis quidem sapiente labore corporis ratione numquam laborum quasi, totam ducimus tempora
+             ex repellendus quisquam ullam, delectus magni nam perferendis est?</p>
+             <h3>john deo</h3>
+             <span>traveler</span>
+             <img src="images/pic-2.jpg" alt="">
+    </div>
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+             veritatis quidem sapiente labore corporis ratione numquam laborum quasi, totam ducimus tempora
+             ex repellendus quisquam ullam, delectus magni nam perferendis est?</p>
+             <h3>john deo</h3>
+             <span>traveler</span>
+             <img src="images/pic-3.jpg" alt="">
+    </div>
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+             veritatis quidem sapiente labore corporis ratione numquam laborum quasi, totam ducimus tempora
+             ex repellendus quisquam ullam, delectus magni nam perferendis est?</p>
+             <h3>john deo</h3>
+             <span>traveler</span>
+             <img src="images/pic-4.jpg" alt="">
+    </div>
+    <div class=" swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+             veritatis quidem sapiente labore corporis ratione numquam laborum quasi, totam ducimus tempora
+             ex repellendus quisquam ullam, delectus magni nam perferendis est?</p>
+             <h3>john deo</h3>
+             <span>traveler</span>
+             <img src="images/pic-5.jpg" alt="">
+    </div>
+    </div>
+    </div>
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+<!-- footer section start -->
+
+<section class="footer">
+   
+    <div class="box-container">
+       
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +912-345-6789</a>
+            <a href="#"><i class="fas fa-phone"></i> +111-222-333-4444</a>
+            <a href="#"><i class="fas fa-envelope"></i> niteshtiwari1232@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Prayagraj, India - 211011</a>
+            
+            
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by<span> Mr. Nitesh Tiwari </span> | All Rights Reserved! </div>
+
+</section>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+
+
+<script src="script.js"></script>
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+
+    
+    <link rel="stylesheet" href="style.css">
+
+
+</head>  
+<body>
+
+<!-- header section start -->
+
+<section class="header">
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+
+<!-- header section end -->
+
+<div class="heading" style="background: url(images/background-2.jpg) no-repeat">
+<h1>packages</h1>
+</div>
+
+
+<!-- packages section starts -->
+
+<section class="packages">
+
+    <h1 class="heading-title">top destinations</h1>
+
+    <div class="box-container">
+
+    <div class="box">
+        <div class="image">
+            <img src="images/img-1.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-2.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+     <div class="box">
+        <div class="image">
+            <img src="images/img-3.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-4.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-5.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-6.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-7.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-8.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-9.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-10.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-11.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+     <div class="box">
+        <div class="image">
+            <img src="images/img-12.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3> adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dignissimos vero doloremque cum unde aspernatur inventore totam, eligendi quidem rerum 
+                ea error velit! Possimus maiores architecto tempora repellat necessitatibus aliquid.</p>
+                <a href="book.php" class="btn"> book now </a>
+        </div>
+     </div>
+
+    <div class="load-more"><span class="btn">load more</span></div> 
+
+
+    </div>
+</section>
+
+
+
+<!-- packages section ends -->
+
+
+
+<!-- footer section start -->
+
+<section class="footer">
+   
+    <div class="box-container">
+       
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +912-345-6789</a>
+            <a href="#"><i class="fas fa-phone"></i> +111-222-333-4444</a>
+            <a href="#"><i class="fas fa-envelope"></i> niteshtiwari1232@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Prayagraj, India - 211011</a>
+            
+            
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by<span> Mr. Nitesh Tiwari </span> | All Rights Reserved! </div>
+
+</section>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+
+
+<script src="script.js"></script>
+</body>
+</html>
